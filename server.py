0d8f9071b37a3d5066346ece8ef208aa7c058355
WhaleDolphin <1728089544@qq.com>
2023-06-26 16:22:02 +0800

    添加了api支持

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
 import socket
-
 import threading
-
 import cfg
-
 import sqlite3
-
-# host = cfg.HOST
-# port = cfg.PORT
-log_level = cfg.LOG_LEVEL
-
 import utils
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
-utils.setup_logging(log_level)
+app = Flask(__name__)
+
+utils.setup_logging(cfg.LOG_LEVEL)
 timestamp = utils.get_timestamp()
+
+@app.route('/users')
+def get_users():
+    db = sqlite3.connect('users.db')
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM users')
+    rows = cursor.fetchall()
+    users = []
+    for row in rows:
+        user = {'id': row[0], 'name': row[1], 'message': row[2]}
+        users.append(user)
+    db.close()
+    return jsonify(users)
+
+if __name__ == '__main__':
+    app.run()
+
 class ChatServer:
     def __init__(self, host, port):
         self.host = "localhost"
@@ -22,30 +35,30 @@ class ChatServer:
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.host, self.port))
         self.clients = []
-        self.db = sqlite3.connect('users.db')
-        self.cursor = self.db.cursor()
-        self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, message TEXT)')
 
     def listen_for_clients(self):
         self.server_socket.listen()
         print(f"Server listening on {self.host}:{self.port}")
         while True:
             client_socket, client_address = self.server_socket.accept()
-            client_socket, client_address = self.server_socket.accept()
             print(f"New client connected: {client_address}")
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
             client_thread.start()
 
     def handle_client(self, client_socket):
+        db = sqlite3.connect('users.db')
+        cursor = db.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, message TEXT)')
+
         # Read from database
-        self.cursor.execute('SELECT * FROM users')
-        rows = self.cursor.fetchall()
+        cursor.execute('SELECT * FROM users')
+        rows = cursor.fetchall()
         for row in rows:
             print(row)
 
         # Write to database
-        self.cursor.execute('INSERT INTO users (name, message) VALUES (?, ?)', ('Alice', 'Hello, world!'))
-        self.db.commit()
+        cursor.execute('INSERT INTO users (name, message) VALUES (?, ?)', ('Alice', 'Hello, world!'))
+        db.commit()
 
         # Handle client messages
         while True:
@@ -59,9 +72,10 @@ class ChatServer:
             except:
                 self.remove_client(client_socket)
                 break
-    
-    def __del__(self):
-        self.db.close()
+
+        # Close database connection
+        cursor.close()
+        db.close()
 
     def broadcast(self, message, sender_socket):
         for client_socket in self.clients:
@@ -72,9 +86,8 @@ class ChatServer:
                     self.remove_client(client_socket)
 
     def remove_client(self, client_socket):
-        if client_socket in self.clients:
-            self.clients.remove(client_socket)
-            print(f"Client disconnected: {client_socket.getpeername()}")
+        self.clients.remove(client_socket)
+        print(f"Client disconnected: {client_socket.getpeername()}")
 
 if __name__ == "__main__":
     server = ChatServer("localhost", 5000)
