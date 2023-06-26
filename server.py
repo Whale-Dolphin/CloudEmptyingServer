2469198edf6c83a3253110479b4269c396cd796d
WhaleDolphin <1728089544@qq.com>
2023-06-26 15:50:43 +0800

    添加了对数据库的支持

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import threading
 
 import cfg
 
+import sqlite3
+
 # host = cfg.HOST
 # port = cfg.PORT
 log_level = cfg.LOG_LEVEL
@@ -20,23 +22,36 @@ class ChatServer:
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.bind((self.host, self.port))
         self.clients = []
+        self.db = sqlite3.connect('users.db')
+        self.cursor = self.db.cursor()
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, message TEXT)')
 
     def listen_for_clients(self):
         self.server_socket.listen()
         print(f"Server listening on {self.host}:{self.port}")
         while True:
             client_socket, client_address = self.server_socket.accept()
+            client_socket, client_address = self.server_socket.accept()
             print(f"New client connected: {client_address}")
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
             client_thread.start()
 
     def handle_client(self, client_socket):
-        self.clients.append(client_socket)
+        # Read from database
+        self.cursor.execute('SELECT * FROM users')
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row)
+
+        # Write to database
+        self.cursor.execute('INSERT INTO users (name, message) VALUES (?, ?)', ('Alice', 'Hello, world!'))
+        self.db.commit()
+
+        # Handle client messages
         while True:
             try:
                 message = client_socket.recv(1024).decode()
                 if message:
-                    print(f"Received message: {message}")
                     self.broadcast(message, client_socket)
                 else:
                     self.remove_client(client_socket)
@@ -44,6 +59,9 @@ class ChatServer:
             except:
                 self.remove_client(client_socket)
                 break
+    
+    def __del__(self):
+        self.db.close()
 
     def broadcast(self, message, sender_socket):
         for client_socket in self.clients:
